028589dfa5e92b37f1f10d8568921685d0b20c9b
xu han <xhan87@gmail.com>
2009-05-04 11:36:29 +0000

    test apps news

--- app/models/apps/new.rb
+++ app/models/apps/new.rb
@@ -1,5 +1,8 @@
 class Apps::New < AppBase
   
   #attr_accessor :title, :body ,:type
+  cms_attr_link :title , :string_1
+  cms_attr_link :body ,  :string_2
+  cms_attr_link :user_id , :froeign_1
   
 end
