899c50b41eece88914cd077e719766d5eadf356d
xu han <xhan87@gmail.com>
2009-05-16 07:16:15 +0000

    helper methods

--- app/helpers/cms/stickers_helper.rb
+++ app/helpers/cms/stickers_helper.rb
@@ -30,16 +30,24 @@ module Cms::StickersHelper
     end
     #edit 
     if page_editable?
-      str_out +=edit_tag(node.id,@page.id)
+      str_out = add_sticker_collection_head(node,@page) +str_out
       str_out = content_tag(:div,str_out,:class => "sticker_collection" )
     end
     return str_out
   end                         
     
+  def add_sticker_collection_head(node,page)
+    edit_content = edit_tag(node.id,page.id)
+    content_tag(:div,edit_content,:class => "sticker_collection_edit")
+  end
+    
   def add_class_if_in_edit content ,sticker,html={:class => "sticker"}
     if page_editable?             
-      edit_content = link_to('v' ,cms_sticker_path(sticker) ) \
-              + link_to('&and;' ,cms_sticker_path(sticker)) 
+      edit_content = link_to(image_tag('view.png') ,cms_sticker_path(sticker) ) \
+              + link_to(image_tag('arrow_up.gif') ,cms_sticker_path(sticker))  \
+              + link_to(image_tag('arrow_down.gif') ,cms_sticker_path(sticker))  \
+              + link_to(image_tag('close.gif') ,cms_sticker_path(sticker))  \
+
       content_tag(:div,edit_content,:class => "sticker_edit") + 
       content_tag(:div,content , html)
     else
@@ -47,8 +55,8 @@ module Cms::StickersHelper
     end
   end
   
-  def edit_tag node_id,page_id
-    link_to("add or select a sticker",cms_stickers_path(:sticker_node_id => node_id,:page_id => page_id))
+  def edit_tag node_id,page_id 
+    link_to(image_tag('add.png'),cms_stickers_path(:sticker_node_id => node_id,:page_id => page_id))
   end
   
   def page_editable?
