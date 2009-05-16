fc2ab803537f4473cc33afc0138d1c90c4b08331
xu han <xhan87@gmail.com>
2009-05-16 12:11:28 +0000

    add sticker manager

--- app/helpers/cms/stickers_helper.rb
+++ app/helpers/cms/stickers_helper.rb
@@ -1,13 +1,12 @@
 module Cms::StickersHelper       
                          
-  #TODO  ， is it proper to place lots of codes here? maybe i need move some to lib later  
   def sticker_tag sym , &block
     str_out = ""
     @templet = @page.templet                                       
     # TODO  if not found a node , whether or not to create a node ?
     # =>    here is default to set to find a exist
-    node = @templet.sticker_nodes.with_name(sym.to_s).first
-    stickers = node.stickers_in_page_id(@page.id)         
+    @node = @templet.sticker_nodes.with_name(sym.to_s).first
+    stickers = @node.stickers_in_page_id(@page.id)         
     if  !stickers.blank?
       for s in stickers                                           
         #text
@@ -30,23 +29,27 @@ module Cms::StickersHelper
     end
     #edit 
     if page_editable?
-      str_out = add_sticker_collection_head(node,@page) +str_out
+      str_out = add_sticker_collection_head + str_out
       str_out = content_tag(:div,str_out,:class => "sticker_collection" )
     end
     return str_out
   end                         
     
-  def add_sticker_collection_head(node,page)
-    edit_content = edit_tag(node.id,page.id)
+  def add_sticker_collection_head
+    edit_content = edit_tag(@node.id,@page.id)
     content_tag(:div,edit_content,:class => "sticker_collection_edit")
   end
     
   def add_class_if_in_edit content ,sticker,html={:class => "sticker"}
-    if page_editable?             
-      edit_content = link_to(image_tag('view.png') ,cms_sticker_path(sticker) ) \
-              + link_to(image_tag('arrow_up.gif') ,cms_sticker_path(sticker))  \
-              + link_to(image_tag('arrow_down.gif') ,cms_sticker_path(sticker))  \
-              + link_to(image_tag('close.gif') ,cms_sticker_path(sticker))  \
+    if page_editable?                                                               
+=begin
+  TODO  on poly model, how to find a special condition like refer = refer_model
+=end      
+      con_sticker = ConSticker.stickers_in_page(@node.id,@page.id).find_by_refer_id(sticker.id)            
+      edit_content = link_to(image_tag('view.png') ,cms_sticker_path(sticker)) +         
+               link_to(image_tag('arrow_up.gif') ,up_cms_sticker_manage_path(con_sticker))  +
+               link_to(image_tag('arrow_down.gif') ,down_cms_sticker_manage_path(con_sticker)) + 
+               link_to(image_tag('close.gif') ,cms_sticker_manage_path(con_sticker),:method => :delete,:confirm => "ps Sticker will remains" )
 
       content_tag(:div,edit_content,:class => "sticker_edit") + 
       content_tag(:div,content , html)
@@ -86,8 +89,8 @@ module Cms::StickersHelper
     @page && @sticker_node
   end
   
-  def method_missing symbol ,*arg
-    "Warning : The Function Name #{symbol.to_s} doesnot Exist!";
-  end
+  # def method_missing symbol ,*arg
+  #   "Warning : The Function Name #{symbol.to_s} doesnot Exist!";
+  # end 
   
 end
